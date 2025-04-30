021ab7f79e0e3ef609583be209e60066d5932155
rom <romikov@gmail.com>
2025-05-01 07:30:40 +1000

    update bootstrap

--- manifests/bootstrap.sh
+++ manifests/bootstrap.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+EXPECTED_CONTEXT="rancher-desktop"
+
+echo "🔍 Checking current kubectl context..."
+CURRENT_CONTEXT=$(kubectl config current-context)
+
+if [[ "$CURRENT_CONTEXT" != "$EXPECTED_CONTEXT" ]]; then
+  echo "You are not connected to the expected Kubernetes context: '$EXPECTED_CONTEXT'"
+  echo "   Current context is: '$CURRENT_CONTEXT'"
+  echo "Run 'kubectl config use-context $EXPECTED_CONTEXT' to switch."
+  exit 1
+fi
+
+echo "Connected to expected context: '$CURRENT_CONTEXT'"
+
+
+kubectl get clusters.management.cattle.io -A
+kubectl label cluster.management.cattle.io/local env=dev
+kubectl get cluster.management.cattle.io/local --show-labels
+
 echo "Creating GitRepo resource to deploy 'grafana' and 'guestbook' via Fleet..."
 
 cat <<EOF | kubectl apply -f -
